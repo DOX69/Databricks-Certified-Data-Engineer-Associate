178804003018d3137372f6b4948f31c376607287
DOX69 <DOX69>
2024-06-05 05:58:17 +0000

    land new data for CDC

---
+++ 4 Production Pipelines/4.3 Land New Data Task.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %run ../Includes/Copy-Datasets
+
+# COMMAND ----------
+
+load_new_json_data()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC let's see the content of this file
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from json.`${dataset.bookstore}/books-cdc/02.json`
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC The operational field is row_status
+# MAGIC
+# MAGIC When we DELETE, we only have one field filled : book_id
+# MAGIC
